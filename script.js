c82325eb97cef0712ebb0b20ecbd132d0137a87c
Mark Preston <markgpreston@gmail.com>
2017-04-30 18:21:14 +0100

    added form and modified div on front page

--- script.js
+++ script.js
@@ -2,25 +2,18 @@ var some_images = new Array ('images/hotel_room.jpg', 'images/kitchen_chairs.jpg
 var index = 1;
 
 function rotateImage() {
-  $('#front_image').fadeOut('slow', function() 
-  {
-    $(this).attr('src', some_images[index]);
-    
-    $(this).fadeIn('slow', function() 
-    {
-      if (index == some_images.length-1)
-      {
+  $('#front_image').fadeOut('slow', function() {
+    $(this).attr('src', some_images[index]);  
+    $(this).fadeIn('slow', function() {
+      if (index == some_images.length-1) {
         index = 0;
-      }
-      else
-      {
+      } else {
         index++;
       }
     });
   });
 } 
 
-$(document).ready(function()
-{
+$(document).ready(function() {
   setInterval (rotateImage, 3000);
 });
