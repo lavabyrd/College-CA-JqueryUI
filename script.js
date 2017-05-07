fe188609451e32e71ab9fe7d55bcb086c4e4fdf9
Mark Preston <markgpreston@gmail.com>
2017-05-07 10:38:34 +0100

    updated images, redesign and updated stylesheets

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var some_images = new Array ('images/hotel_room.jpg', 'images/kitchen_chairs.jpg', 'images/kitchen_food.jpg');
+var some_images = new Array('images/beach.jpg', 'images/sleep.jpg', 'images/coffee.jpg');
 var index = 1;
 
 function rotateImage() {
   $('#front_image').fadeOut('slow', function() {
-    $(this).attr('src', some_images[index]);  
+    $(this).attr('src', some_images[index]);
     $(this).fadeIn('slow', function() {
-      if (index == some_images.length-1) {
+      if (index == some_images.length - 1) {
         index = 0;
       } else {
         index++;
       }
     });
   });
-} 
+}
 
 $(document).ready(function() {
-  setInterval (rotateImage, 3000);
+  setInterval(rotateImage, 3000);
 });
